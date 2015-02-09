01639d1b4659238eafeeb6b9cfb3f17de8a50621
andrew <andrew@omalley.id.au>
2015-02-10 04:38:19 +1100

    Add StopWatch

---
+++ src/main/kotlin/com/github/andrewoma/kommon/util/StopWatch.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2015 Andrew O'Malley
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.andrewoma.kommon.util
+
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.*
+
+/**
+ * StopWatch is a simple timer. Multiple timings can be accumulated by calling start() and stop()
+ * in sequence.
+ */
+public class StopWatch(val currentTime: () -> Long = { System.nanoTime() }) {
+    private var start: Long = 0
+    private var elapsedNanoseconds: Long = 0
+    private var running: Boolean = false
+
+    public fun start(): StopWatch {
+        check(!running, "The stop watch is already started")
+        start = currentTime()
+        running = true
+        return this
+    }
+
+    public inline fun <R> time(f: () -> R): R {
+        start()
+        try {
+            return f()
+        } finally {
+            stop()
+        }
+    }
+
+    public fun stop(): StopWatch {
+        check(running, "The stop watch is already stopped")
+        running = false
+        elapsedNanoseconds += currentTime() - start
+        return this
+    }
+
+    public fun reset(): StopWatch {
+        elapsedNanoseconds = 0
+        running = false
+        return this
+    }
+
+    public fun elapsed(unit: TimeUnit): Long {
+        val elapsed = if (running) elapsedNanoseconds + currentTime() - start else elapsedNanoseconds
+        return unit.convert(elapsed, NANOSECONDS)
+    }
+
+    override fun toString() = toString(MILLISECONDS)
+
+    public fun toString(unit: TimeUnit, precision: Int = 3): String {
+        val value = elapsed(NANOSECONDS).toDouble() / NANOSECONDS.convert(1, unit)
+
+        return "%.${precision}f %s".format(value, unit.abbreviation())
+    }
+}
+
+public fun TimeUnit.abbreviation(): String {
+    return when (this) {
+        NANOSECONDS -> "ns"
+        MICROSECONDS -> "\u03bcs"
+        MILLISECONDS -> "ms"
+        SECONDS -> "s"
+        MINUTES -> "m"
+        HOURS -> "h"
+        DAYS -> "d"
+        else -> name()
+    }
+}
---
+++ src/test/kotlin/com/github/andrewoma/kommon/util/StopWatchTest.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2015 Andrew O'Malley
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.andrewoma.kommon.util
+
+import org.junit.Test as test
+import kotlin.test.assertEquals
+import java.util.concurrent.TimeUnit
+
+class ProcessTest() {
+    var time = 0L
+    val stopWatch = StopWatch({ time })
+
+    test fun `Elapsed should equal start - stop`() {
+        stopWatch.start()
+        time = 10
+        stopWatch.stop()
+        time = 20
+        assertEquals(10L, stopWatch.elapsed(TimeUnit.NANOSECONDS))
+    }
+
+    test fun `Elapsed should equal start - current while running`() {
+        stopWatch.start()
+        time = 10
+        assertEquals(10L, stopWatch.elapsed(TimeUnit.NANOSECONDS))
+    }
+
+    test fun `Elapse should accumulate`() {
+        stopWatch.start()
+        time = 10
+        stopWatch.stop()
+        time = 20
+        stopWatch.start()
+        time = 25
+        stopWatch.stop()
+        time = 30
+
+        assertEquals(15L, stopWatch.elapsed(TimeUnit.NANOSECONDS))
+    }
+
+    test fun `Time should equal function execution time`() {
+        time = 10
+        stopWatch.time {
+            time = 25
+        }
+        assertEquals(15L, stopWatch.elapsed(TimeUnit.NANOSECONDS))
+    }
+
+    test fun `Reset should clear timer`() {
+        stopWatch.start()
+        time = 10
+        stopWatch.stop()
+        assertEquals(10L, stopWatch.elapsed(TimeUnit.NANOSECONDS))
+
+        stopWatch.reset()
+        assertEquals(0L, stopWatch.elapsed(TimeUnit.NANOSECONDS))
+    }
+
+    test fun `Elapsed should convert units`() {
+        stopWatch.start()
+        time = TimeUnit.SECONDS.toNanos(2)
+        stopWatch.stop()
+        assertEquals(2L, stopWatch.elapsed(TimeUnit.SECONDS))
+        assertEquals(2000L, stopWatch.elapsed(TimeUnit.MILLISECONDS))
+    }
+
+    test fun `toString should display with precision and units`() {
+        stopWatch.start()
+        time = TimeUnit.SECONDS.toNanos(2)
+        stopWatch.stop()
+        assertEquals("0.033 m", stopWatch.toString(TimeUnit.MINUTES))
+        assertEquals("2.000 s", stopWatch.toString(TimeUnit.SECONDS))
+        assertEquals("2000.000 ms", stopWatch.toString())
+        assertEquals("2 s", stopWatch.toString(TimeUnit.SECONDS, 0))
+    }
+}
